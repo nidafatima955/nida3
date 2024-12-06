11b60f47029732e2a09296ddc77664f672627c5c
nidafatima955 <nidaf9750@gmail.com>
2024-12-06 15:12:27 -0800

    first commit

---
+++ file.js
@@ -0,0 +1,12 @@
+function login(){
+    let a=document.getElementById('email').value
+    let b=document.getElementById('pass').value
+
+    if(a==="nidaf9750@gmail.com" && b==="nidafatima"){
+        alert('login successfully')
+    }
+
+else{
+    alert('enter a valid email and address')
+}
+}
